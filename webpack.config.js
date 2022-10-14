c1a8eee5d43b13c149b67ccd634b92c917f8d702
Yuri Nunes Atzler <atzleryuri@gmail.com>
2022-10-14 11:20:40 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./page/index.html",
+      hash: true,
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./assets", to: "./assets" }],
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 3000,
+  },
+};
